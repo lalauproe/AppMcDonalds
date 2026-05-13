122720823581c85be05ce95f20c9800e0aaa5a6d
LAURA DE CASTR NEVES PROENCA <proenca.3977@aluno.pr.senac.br>
2026-05-13 16:51:17 -0300

    renomeando as imgs do projeto

---
+++ data/images.ts
@@ -0,0 +1,30 @@
+import type { ImageSourcePropType } from 'react-native';
+
+export const AppImages = {
+    estabelecimento: require('@/assets/images/estabelecimento.png') as ImageSourcePropType,
+    logo: require('@/assets/images/logo.png') as ImageSourcePropType,
+    Agua: require('@/assets/images/agua.png') as ImageSourcePropType,
+    fanta: require('@/assets/images/fanta.png') as ImageSourcePropType,
+    cocacola: require('@/assets/images/coca-cola.png') as ImageSourcePropType,
+    batatapequena: require('@/assets/images/batata-pequena.png') as ImageSourcePropType,
+    batatamedia: require('@/assets/images/batata-media.png') as ImageSourcePropType,
+    batatagrande: require('@/assets/images/batata-grande.png') as ImageSourcePropType,
+    casquinhabaunilha: require('@/assets/images/casquinha-baunilha.png') as ImageSourcePropType,
+    casquinhachocolate: require('@/assets/images/casquinha-chocolate.png') as ImageSourcePropType,
+    casquinhamista: require('@/assets/images/casquinha-mista.png') as ImageSourcePropType,
+    bigmac: require('@/assets/images/Big-Mac.png') as ImageSourcePropType,
+    cheddarmcmelt: require('@/assets/images/Cheddar-McMelt.png') as ImageSourcePropType,
+    mcnificio: require('@/assets/images/McNífico.png') as ImageSourcePropType,
+    duplocheggarmcmelt: require('@/assets/images/Duplo-Cheddar-McMelt.png') as ImageSourcePropType,
+    duploquarterao: require('@/assets/images/Duplo-Quarteirao.png') as ImageSourcePropType,
+    combonionrings: require('@/assets/images/combo-Brabo-Melt-Onion-Rings.png') as ImageSourcePropType,
+    combocheddar: require('@/assets/images/combo-cheddar-duplo.png') as ImageSourcePropType,
+    combobigmac: require('@/assets/images/combo-Duplo-Big-Mac.png') as ImageSourcePropType,
+    combochicken: require('@/assets/images/combo-McCrispy-Chicken-Elite.png') as ImageSourcePropType,
+    
+
+
+
+
+
+}
